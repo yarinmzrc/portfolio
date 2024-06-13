2490eb0ad250ebb1d15d96bf8c0224806abc956c
yarinmzrc <yarinmzrc@gmail.com>
2024-06-13 19:51:18 +0300

    added buttton transitions

--- components/Button.tsx
+++ components/Button.tsx
@@ -2,7 +2,8 @@ import { cn } from "components/utils";
 import { ButtonHTMLAttributes } from "react";
 
 const variantClasses = {
-  primary: "py-1 px-2 rounded-[4px] border border-primary text-primary",
+  primary:
+    "py-1 px-2 rounded-[4px] border border-primary text-primary hover:bg-primary hover:skew-y-1 hover:text-customBlue transition-all duration-200",
   secondary: "py-1 px-4 rounded-full border border-primary text-primary",
 };
 
