8422e1df2c11db12227f05d27706cd303b6ea5e8
Yarin Mizrachi <yarin@beprofit.co>
2024-06-13 11:44:23 +0300

    updated classes + import About component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import About from "components/components/About";
 import NavBar from "components/components/NavBar";
 
 export default function Home() {
   return (
-    <main className="container mx-auto py-12 min-h-full text-secondary-light">
+    <main className="container mx-auto py-12 px-6 md:px-12 lg:px-20 min-h-full text-secondary-light flex flex-col gap-20 md:gap-40">
       <NavBar />
+      <About />
     </main>
   );
 }
