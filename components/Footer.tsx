da4187440c57d56af19b78d909c4f47ba76c43ba
yarinmzrc <yarinmzrc@gmail.com>
2024-06-16 16:29:11 +0300

    fixed style

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,7 +8,7 @@ function Footer() {
   const iconClasses =
     "h-4 w-4 transition-all duration-200 hover:scale-110 hover:text-white";
   return (
-    <footer className="items-start-center flex flex-col gap-4">
+    <footer className="flex flex-col items-center gap-4">
       <p className="text-sm">Designed & Built by Yarin Mizrachi</p>
       <div className="flex items-center justify-center gap-4">
         <Link href={GITHUB_LINK} target="_blank">
