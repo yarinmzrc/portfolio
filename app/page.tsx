c60c3f0a4f6b8ad0c064a5c5a18d25fa2220ce66
yarinmzrc <yarinmzrc@gmail.com>
2024-06-16 16:17:08 +0300

    fixed spacing

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Page() {
   return (
     <div>
       <NavBar />
-      <main className="container mx-auto flex min-h-full flex-col gap-32 px-6 py-12 text-secondary-light md:gap-40 md:px-12 lg:px-20">
+      <main className="container mx-auto flex min-h-full flex-col gap-40 px-6 pb-12 pt-32 text-secondary-light md:px-12 md:pt-12 lg:px-20">
         <Home />
         <About />
         <Experience />
--- components/Contact.tsx
+++ components/Contact.tsx
@@ -13,7 +13,7 @@ function Contact() {
   return (
     <section
       id="contact"
-      className="flex flex-col items-center pb-20 pt-10 text-center md:pb-32 md:pt-44"
+      className="flex flex-col items-center pt-10 text-center md:pb-32 md:pt-44"
     >
       <h2 className="mb-6 text-3xl font-semibold text-secondary-light md:text-6xl">
         {title}
