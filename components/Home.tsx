e7b489c9c989dbd6579654c960d9e3aac13b4500
yarinmzrc <yarinmzrc@gmail.com>
2024-06-16 16:22:11 +0300

    fixed spacing

--- components/Home.tsx
+++ components/Home.tsx
@@ -9,10 +9,10 @@ function Home() {
   return (
     <section id="home" className="flex flex-col gap-4 md:pt-24">
       <p className="text-primary">{welcomeTitle}</p>
-      <h1 className="text-3xl font-bold text-secondary-light sm:text-4xl md:text-7xl">
+      <h1 className="text-3xl font-bold text-secondary-light sm:text-4xl md:text-6xl">
         {title}
       </h1>
-      <h2 className="text-xl font-semibold text-secondary sm:text-2xl md:text-6xl">
+      <h2 className="text-xl font-semibold text-secondary sm:text-2xl md:text-5xl">
         {subtitle}
       </h2>
       <p className="mb-4 max-w-[33rem] text-base text-secondary md:text-lg">
